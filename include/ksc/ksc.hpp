2e1e1a806184cf6c0e40ff836d4a5b362542073c
Nicolas <kessler@fzi.de>
2018-10-21 22:05:35 +0200

    prototype for active vessel query

--- include/ksc/ksc.hpp
+++ include/ksc/ksc.hpp
@@ -3,7 +3,13 @@
 
 #include "ksc/visibility_control.h"
 #include "rclcpp/rclcpp.hpp"
+
 #include "std_msgs/msg/string.hpp"
+#include "roar_msg/msg/active_vessel_reply.hpp"
+
+#include <krpc.hpp>
+#include <krpc/services/krpc.hpp>
+#include <krpc/services/space_center.hpp>
 
 namespace ksc
 {
@@ -11,14 +17,21 @@ namespace ksc
 class Ksc : public rclcpp::Node
 {
 public:
-  Ksc();
+  Ksc(krpc::Client conn);
 
   virtual ~Ksc();
 
 private:
 
-  void topic_callback(const std_msgs::msg::String::SharedPtr msg);
-  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+  bool vessel_name_in_list(krpc::services::SpaceCenter::Vessel vessel, std::vector<krpc::services::SpaceCenter::Vessel> vessel_list);
+  void active_vessel_query_callback(const std_msgs::msg::String::SharedPtr msg);
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr active_vessel_query_subscription;
+  rclcpp::Publisher<roar_msg::msg::ActiveVesselReply>::SharedPtr active_vessel_query_publisher;
+
+  krpc::Client conn;
+  krpc::services::KRPC krpc;
+  krpc::services::SpaceCenter sc;
 };
 
 }  // namespace ksc
--- src/ksc.cpp
+++ src/ksc.cpp
@@ -1,35 +1,72 @@
 #include "ksc/ksc.hpp"
 
 #include <iostream>
-#include <krpc.hpp>
-#include <krpc/services/krpc.hpp>
 
 using std::placeholders::_1;
+using krpc::services::SpaceCenter;
 
 // functionality goes into this file
 
 namespace ksc
 {
 
-Ksc::Ksc(): Node("ros_ksc_node")
+Ksc::Ksc(krpc::Client conn): Node("ros_ksc_node"), conn{conn}, krpc(&conn), sc(&conn)
 {
     // constructor
-    auto conn = krpc::connect("ros_ksc_node", "127.0.0.1", 1000, 1001);
-    krpc::services::KRPC krpc(&conn);
-//    krpc::services::SpaceCenter sc(&conn);
     std::cout << krpc.get_status().version() << std::endl;
 
-    subscription_ = this->create_subscription<std_msgs::msg::String>(
-          "topic", std::bind(&Ksc::topic_callback, this, _1));
+    active_vessel_query_subscription = this->create_subscription<std_msgs::msg::String>(
+          "active_vessel_query", std::bind(&Ksc::active_vessel_query_callback, this, _1));
+    active_vessel_query_publisher = this->create_publisher<roar_msg::msg::ActiveVesselReply>("active_vessel_reply");
 }
 
 Ksc::~Ksc()
 {
 }
 
-void Ksc::topic_callback(const std_msgs::msg::String::SharedPtr msg)
+bool Ksc::vessel_name_in_list(SpaceCenter::Vessel vessel, std::vector<SpaceCenter::Vessel> vessel_list){
+
+    for (SpaceCenter::Vessel cur_vessel : vessel_list) {
+        if (cur_vessel.name() == vessel.name()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+void Ksc::active_vessel_query_callback(const std_msgs::msg::String::SharedPtr msg)
 {
-  RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+    /*
+     * retrieves the active vessel and if the msg data is not empty, checks,
+     * if the active vessel's name matches.
+     * It will also publish a bool, which will be true if and only if there
+     * is a second vessel with the active vessel's name.
+     */
+    bool matches;
+
+    std::string vessel_name = msg->data.c_str();
+    std::vector<SpaceCenter::Vessel> all_vessels = sc.vessels();
+    SpaceCenter::Vessel active_vessel = sc.active_vessel();
+
+    // check, if active vessel name is unique:
+    bool vesselIsUnique = this->vessel_name_in_list(active_vessel, all_vessels);
+
+    if (vessel_name.empty()){
+        // just publish active vessel, this will match on any vessel's name as true
+        matches = true;
+    }
+    else {
+        // check, if the active vessel's name matches, then publish
+        matches = active_vessel.name() == vessel_name;
+    }
+    RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg->data.c_str());
+
+    // publish response
+    roar_msg::msg::ActiveVesselReply reply = roar_msg::msg::ActiveVesselReply();
+    reply.name = vessel_name;
+    reply.unique = vesselIsUnique;
+    reply.matches = matches;
+    active_vessel_query_publisher->publish(reply);
 }
 
 }  // namespace ksc
